315253de14309dbde63a1947447dd2e011175818
Vitali Herasimenka <vitttold@inbox.ru>
2021-03-12 18:44:28 +0300

    add: solition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  let a = 0;
+  
+  if (arguments.length < 1 || array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < a) {
+     a = array[i];
+     }
+    }
+return a
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  let a = 0;
+  
+  if (arguments.length < 1 || array.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > a) {
+     a = array[i];
+     }
+    }
+return a
+ }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  let sum = 0;
+
+  if (arguments.length < 1 || array.length === 0) {
+    return 0;
+  }
+    for (let i = 0; i < array.length; i++) {
+      sum = array[i] + sum;
+    }
+return sum/array.length
 }
